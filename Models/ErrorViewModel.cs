3c9722d3b222bfd6e3b92640221e9f25a50f9269
Toby Sullivan (AQI) <toby.sullivan@aquaticinformatics.com>
2026-04-20 20:17:51 +0000

    Mtt live demo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace agentmodedemo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
